178b1d012981237d010238d2193a38de6fdb3cce
Ben Abitbol <Benjiab@hotmail.com>
2018-07-27 15:17:49 +0300

    fixing more typos..

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === "production") {
-  module.exports = require("./keys_prodkeys_pod");
+  module.exports = require("./keys_prod");
 } else {
   module.exports = require("./keys_dev");
 }
