9a858855d784e00bf9e98a477610f2ddceb61318
Ben Abitbol <Benjiab@hotmail.com>
2018-07-27 15:12:17 +0300

    fixing typo

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require("body-Parser");
+const bodyParser = require("body-parser");
 const passport = require("passport");
 const path = require("path");
 
